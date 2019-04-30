9b682ee438af8b024e991d8b7d9e327959af5aa5
mohassan <vanmowha@gmail.com>
2019-04-30 09:25:58 +0300

    deploying to heroku

--- app/models.py
+++ app/models.py
@@ -30,7 +30,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(255), unique=True, index=True)
     password_hash = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
-    blogs = db.relationship('Blog', backref='blogs', lazy="dynamic")
+    blogs = db.relationship('Blog', backref='blog', lazy="dynamic")
     comments = db.relationship('Comment', backref='comments', lazy="dynamic")
 
     @property
